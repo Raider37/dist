99246bfcd452fa906478ccc9efdbd65c35630ae9
Raider37 <56130875+Raider37@users.noreply.github.com>
2021-01-12 18:12:37 +0300

    Add files via upload

---
+++ precache-manifest.708beca712dfc56b001ad68914ec710e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd974f38e484a09d6133",
+    "url": "/css/app.c8e6e1d8.css"
+  },
+  {
+    "revision": "87a7d8002881654b3d07",
+    "url": "/css/chunk-vendors.6950a743.css"
+  },
+  {
+    "revision": "7c34f59e1e082ed0660ed672a35e454c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7604cd3705c7d36fa1",
+    "url": "/js/about.8bc1cf4f.js"
+  },
+  {
+    "revision": "cd974f38e484a09d6133",
+    "url": "/js/app.4dff851a.js"
+  },
+  {
+    "revision": "87a7d8002881654b3d07",
+    "url": "/js/chunk-vendors.326bd67b.js"
+  },
+  {
+    "revision": "93cfd8f7bf1cf4cf0478520b1fb670a2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
